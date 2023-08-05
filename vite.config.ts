2d6c49236d46e639d9b7b6860635717e8b975022
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2023-08-05 09:10:11 +0900

    Create Github Actions for deployments

--- vite.config.ts
+++ vite.config.ts
@@ -3,36 +3,40 @@ import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
 
 // check if we are building the library
-const isLib = process.env.BUILD_LIB;
+const isBuildingLibrary =
+  process.env.BUILD_LIB !== undefined && process.env.BUILD_LIB === 'true';
+
+const plugins = [vue()];
+// if we are building the library, add the dts plugin to generate the typings
+if (isBuildingLibrary) {
+  plugins.push(dts({ exclude: ['src/examples/**/*', 'src/lib/*.spec.ts'] }));
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    dts({ exclude: ['src/examples/**/*', 'src/lib/*.spec.ts'] }),
-    vue(),
-  ],
-  build:
-    isLib != null && isLib === 'true'
-      ? {
-          lib: {
-            entry: 'src/lib/index.ts',
-            name: 'VueTaskManagement',
-            fileName: (format) => `vue-task-management.${format}.js`,
-          },
-          rollupOptions: {
-            external: ['vue'],
-            output: {
-              globals: {
-                vue: 'Vue',
-              },
+  base: isBuildingLibrary ? '/' : '/vue-task-management/',
+  plugins,
+  build: isBuildingLibrary
+    ? {
+        lib: {
+          entry: 'src/lib/index.ts',
+          name: 'VueTaskManagement',
+          fileName: (format) => `vue-task-management.${format}.js`,
+        },
+        rollupOptions: {
+          external: ['vue'],
+          output: {
+            globals: {
+              vue: 'Vue',
             },
           },
-        }
-      : {
-          outDir: 'dist/examples',
-          assetsDir: './',
-          rollupOptions: {
-            input: 'src/examples/main.ts',
-          },
         },
+      }
+    : {
+        outDir: 'dist/examples',
+        assetsDir: './',
+        rollupOptions: {
+          input: ['src/examples/main.ts', './index.html'],
+        },
+      },
 });
